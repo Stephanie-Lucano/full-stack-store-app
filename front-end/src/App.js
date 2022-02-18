a35a1d1cbf450c777b72c154997ada2e9b27271a
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-02-18 16:04:35 -0500

    test

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 const API = process.env.REACT_APP_API_URL;
 
+console.log(API);
 function App() {
   const [days, setDays] = useState([]);
   useEffect(() => {
