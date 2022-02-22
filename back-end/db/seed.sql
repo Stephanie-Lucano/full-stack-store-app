a057e851d19d214528bc8ccc2ae8d4607d7d5bd9
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-02-22 10:59:02 -0500

    formatting money removed in sql

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -3,7 +3,7 @@
 INSERT INTO products (name, price, rating, featured, image) VALUES
 ('Shelf unit', 129, 4, true, 'https://www.ikea.com/us/en/images/products/kallax-shelf-unit-white__0644182_pe702466_s5.jpg?f=xl'),
 ('Convertible bed', 799, 5, true, 'https://www.ikea.com/us/en/images/products/friheten-sleeper-sectional-3-seat-w-storage-hyllie-beige__0690252_pe723176_s5.jpg?f=xl'),
-('China cabinet', 1,059, 4, false, 'https://www.ikea.com/us/en/images/products/havsta-storage-with-sliding-glass-doors-white__0720067_pe732392_s5.jpg?f=xl'),
+('China cabinet', 1059, 4, false, 'https://www.ikea.com/us/en/images/products/havsta-storage-with-sliding-glass-doors-white__0720067_pe732392_s5.jpg?f=xl'),
 ('Mordern arch shaped framed black standing mirror', 178, 3, true, 'https://images.thdstatic.com/productImages/a48f82f6-6ae3-4597-92f7-5550fc39d089/svn/pexfix-floor-mirrors-us-lj163ps011-bk-64_400.jpg'),
 ('Adjustable height standing desk', 370, 4, true, 'https://pyxis.nymag.com/v1/imgs/6c4/0a4/6b07ea83f095f7f3be98302624616013bb.2x.rsquare.w600.jpg'),
-('Dining table', 1,599, 5, true, 'https://assets.pbimgs.com/pbimgs/ab/images/dp/wcm/202134/0914/toscana-dining-table-z.jpg');
+('Dining table', 1599, 5, true, 'https://assets.pbimgs.com/pbimgs/ab/images/dp/wcm/202134/0914/toscana-dining-table-z.jpg');
